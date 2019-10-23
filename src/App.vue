7cdacb1e77669ea3942017e7bc07346ec781022b
amm <huangxin8697@gmail.com>
2019-10-23 12:46:31 +0800

    feat: 目录

--- src/App.vue
+++ src/App.vue
@@ -40,55 +40,104 @@
       <Row>
         <Col span="18">
           <div style="border-right:1px solid #e8eaec">
-            <h1>写在前面</h1>
+            <h1 id="preface">写在前面</h1>
             <p>- 这是我自己开发的框架.在此之前我一直使用的是koa/egg框架进行开发,但是过程中发现有很多不尽我意的地方,为了能够更顺手的开发web程序,我决定自己开发</p>
             <p>
-              - 名字的来源是两个朋友的名字联合,因此我称它为
-              <b>蟹黄堡</b>
+              - 名字的来源是两个朋友的名字联合,因此我称它为<b>蟹黄堡</b>
             </p>
             <p>
-              - 开发的中心思想是完全符合
-              <b>我</b>的开发习惯,以及尽可能的
-              <b>模块化</b>和附带
-              <b>智能提示</b>
+              - 开发的中心思想是完全符合<b>我</b>的开发习惯,以及尽可能的<b>模块化</b>和附带<b>智能提示</b>
             </p>
-            <h1>优势</h1>
+            <h1 id="advantage">优势</h1>
             <p># 组件\请求\工具Load在上下文中</p>
             <p># 可以自定义loader及typings</p>
             <p># 非常夸张的深层嵌套洋葱圈模型</p>
             <p># 企图可拆分微服务模块</p>
             <p># 自动重启以及更多的开放性api</p>
             <p># 以约束定义规则</p>
-            <h1>安装及快速开始</h1>
+            <h1 id="init">安装及快速开始</h1>
             <div class="code">
               <pre>{{code.init}}</pre>
             </div>
-            <h1>目录树</h1>
-            <h2>标准目录</h2>
+            <h1 id="tree">目录树</h1>
+            <h2 id="common_tree">标准目录</h2>
             <p>常用的目录结构</p>
             <div class="common-tree">
               <Tree :data="fileTree_1"></Tree>
             </div>
-            <h2>子模块目录</h2>
+            <h2 id="module_tree">子模块目录</h2>
             <p>业务需要有嵌套模块的目录结构,此时中间件也是嵌套的</p>
             <div>
               <Tree :data="fileTree_2"></Tree>
             </div>
-						<h1>入口文件</h1>
-						<p>项目从这里开始,并启动一个蟹黄堡实例</p>
-						<div class="code">
+            <h1 id="enter">入口文件</h1>
+            <p>项目从这里开始,并启动一个蟹黄堡实例</p>
+            <div class="code">
               <pre>{{code.app}}</pre>
             </div>
-						<h1>Controller</h1>
-						<p>这里是路由和功能组合文件</p>
-						<p>我是用<b>[new Symbol()]</b>作为<b>Function</b>名称.因为我认为这里的索引是路由的地址,方法不应该被其他形式的调用</p>
-						<div class="code">
+            <h1 id="module">Module</h1>
+            <p>这里是面包片,功能都在这里组合</p>
+            <div class="code">
+              <pre>{{code.module}}</pre>
+            </div>
+            <h1 id="controller">Controller</h1>
+            <p>这里是路由和功能组合文件</p>
+            <p>
+              我是用
+              <b>[new Symbol()]</b>作为
+              <b>Function</b>名称.因为我认为这里的索引是路由的地址,方法不应该被其他形式的调用
+            </p>
+            <div class="code">
               <pre>{{code.controller}}</pre>
+            </div>
+
+            <h1 id="service">Service</h1>
+            <p>这里是业务功能模块</p>
+            <p>
+              这里可以在上下文拿到请求的
+              <b>body</b>,
+              <b>file</b>,
+              <b>query</b>,
+              <b>header</b>
+            </p>
+            <div class="code">
+              <pre>{{code.service}}</pre>
+            </div>
+
+            <h1 id="middleware">Middleware</h1>
+            <p>这里是请求中间件</p>
+            <p>中间件的加载顺序为底层base.module -> controller -> router</p>
+            <p>多模块的中间件的加载顺序base.module -> 子模块base.module,往下与基本顺序对齐</p>
+            <h2 id="module_middleware">module级别中间件</h2>
+            <div class="code">
+              <pre>{{code.middleware.module}}</pre>
+            </div>
+            <h2 id="controller_middleware">controller及router级别中间件</h2>
+            <div class="code">
+              <pre>{{code.middleware.controller}}</pre>
             </div>
           </div>
         </Col>
         <Col span="6">
-          <div style="padding:0 16px">这里是目录</div>
+          <div class="catalog">
+            <Anchor show-ink offset-top="60">
+              <AnchorLink href="#preface" title="写在前面" />
+              <AnchorLink href="#advantage" title="优势" />
+              <AnchorLink href="#init" title="安装及快速开始" />
+              <AnchorLink href="#tree" title="目录树">
+                <AnchorLink href="#common_tree" title="标准目录" />
+                <AnchorLink href="#module_tree" title="子模块目录" />
+              </AnchorLink>
+              <AnchorLink href="#enter" title="入口文件" />
+              <AnchorLink href="#module" title="Module" />
+              <AnchorLink href="#controller" title="Controller" />
+              <AnchorLink href="#service" title="Service" />
+              <AnchorLink href="#middleware" title="Middleware">
+                <AnchorLink href="#module_middleware" title="module级别中间件" />
+                <AnchorLink href="#controller_middleware" title="controller及router级别中间件" />
+              </AnchorLink>
+            </Anchor>
+          </div>
         </Col>
       </Row>
     </div>
@@ -186,5 +235,8 @@ h3 {
 	border-left: 3px solid black;
 	line-height: 24px;
 	font-size: 16px;
+}
+.catalog {
+	padding: 16px;
 }
 </style>
--- src/code/index.ts
+++ src/code/index.ts
@@ -1,23 +1,69 @@
 export default {
 	init: `npm i -g krabby-patty\nkrabby-patty init demo\ncd demo\nnpm run dev`,
 	app: `import {krabbyPatty} from "krabby-patty"\n\nkrabbyPatty({config:{baseDir:__dirname,port:3000}})`,
-	controller: `
-import {Controller,Router,Get} from 'krabby—patty'
+	module:
+		`import {Module} from 'krabby—patty
+export default class BaseModule extends Module{}`,
+	controller: 
+	`import {Router,Get} from 'krabby—patty'
 import BaseModule from '../base.module'
 
 @Router('')
 export default class DemoController extends BaseModule{
-
-   readonly middleware=[testFunction]
 	 
    @Get('/service')
    [new Symbol()](){
       return this.service.demo.dodo()
    }
-	 
-   @Get({path:'/service',middleware:[]})
-   [new Symbol()](){
-      return this.service.demo.dodo()
-   }
-}`
+}`,
+	service:
+		`import BaseModule from '../base.module';
+
+export default class DemoService extends BaseModule{
+  public async dodo(){
+    console.log('成功');
+    return true
+  }		
+}`,
+	middleware: {
+		module:
+			`
+import {Module} from 'krabby—patty'
+
+function testFunction(req, res, next) {
+  console.log("中间件2");
+  next();
+  console.log("中间件2s");
+}
+
+export default class BaseModule extends Module{
+
+  readonly middleware = [testFunction]
+}`,
+		controller:
+			`import {Get,Router} from 'krabby—patty'
+import BaseModule from '../base.module'
+
+function testFunction3(req,res,next){
+  console.log('中间件3');
+  next()
+  console.log('中间件3s');
+}
+
+function testFunction4(req,res,next){
+  console.log('中间件4');
+  next()
+  console.log('中间件4s')
+}
+
+@Router('/user')
+export default class DemoController extends BaseModule {
+
+  readonly middleware=[testFunction3]
+
+  @Get({ path: '/create', middleware:[testFunction4]})
+  [Symbol()]() {return}
+}
+			`
+	}
 }
